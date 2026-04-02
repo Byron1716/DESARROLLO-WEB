2ed2afcf767431a9d60863b18a6c547aee98ec7b
Byron Rosado <byron2817@hotmail.com>
2026-04-01 22:39:24 -0500

    CORRECCIÓN PARA RENDER

--- SEMANA 9/inventario/db.py
+++ SEMANA 9/inventario/db.py
@@ -7,12 +7,26 @@ from flask import g
 
 DATABASE = r"C:\Users\Byron Rosado\DESARROLLO WEB\SEMANA 9\clinica.db"
 
+# inventario/db.py
+import os
+import sqlite3
+from flask import g, current_app
+
 def get_db():
     if "db" not in g:
-        print("🧠 USANDO BASE:", DATABASE)
-        g.db = sqlite3.connect(DATABASE)
+        # ✅ Ruta PORTABLE (Windows, Linux, Render)
+        db_path = os.path.join(current_app.root_path, "clinica.db")
+        g.db = sqlite3.connect(db_path)
         g.db.row_factory = sqlite3.Row
     return g.db
+
+def close_db(e=None):
+    """
+    Cierra la conexión SQLite al finalizar la request
+    """
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
 
 def close_db(e=None):
     """
